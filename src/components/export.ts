6c90d73df9046010e59ff54c04f91acb21247708
somayeh.rohani <somayeh.rouhani.kubakgroup.com>
2021-05-15 17:39:38 +0430

    upload file component

--- src/components/export.ts
+++ src/components/export.ts
@@ -14,6 +14,7 @@ import HorizontalSelect from "./HorizontalSelect.vue";
 import RadioButton from "./RadioButton.vue";
 import Modal from "./Modal.vue";
 import InputTag from "./InputTag.vue";
+import Uploader from "./Uploader.vue";
 
 export default {
     Button: Button,
@@ -31,6 +32,7 @@ export default {
     HorizontalSelect: HorizontalSelect,
     RadioButton: RadioButton,
     Modal: Modal,
-    InputTag:InputTag
+    InputTag: InputTag,
+    Uploader: Uploader
 
 }

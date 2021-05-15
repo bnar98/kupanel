a26b20851f510e3b92da314cf2c32a317f886fe8
namdari <sohaila.namdari@gmail.com>
2021-05-15 17:08:52 +0430

    iput code done

--- src/components/export.ts
+++ src/components/export.ts
@@ -14,6 +14,7 @@ import HorizontalSelect from "./HorizontalSelect.vue";
 import RadioButton from "./RadioButton.vue";
 import Modal from "./Modal.vue";
 import InputTag from "./InputTag.vue";
+import InputCode from "./InputCode.vue"
 
 export default {
     Button: Button,
@@ -31,6 +32,7 @@ export default {
     HorizontalSelect: HorizontalSelect,
     RadioButton: RadioButton,
     Modal: Modal,
-    InputTag:InputTag
+    InputTag:InputTag,
+    InputCode:InputCode
 
 }

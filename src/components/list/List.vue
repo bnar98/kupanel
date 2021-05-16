9db002b31639b75485c16093b40b630b860201d9
somayeh.rohani <somayeh.rouhani.kubakgroup.com>
2021-05-16 11:04:46 +0430

    list component

---
+++ src/components/list/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="list-striped">
+    <slot></slot>
+  </ul>
+</template>
+<style lang="scss" scoped>
+.list-striped {
+  @apply p-0 m-0 list-none w-full;
+
+  .list-item {
+    @apply p-2.5 w-full;
+
+    &:nth-child(odd) {
+      @apply bg-table-row-hover-background-color;
+    }
+
+    &:nth-child(even) {
+      @apply bg-transparent;
+    }
+  }
+}
+</style>

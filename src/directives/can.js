4620c8d6690ea502cda82676eedc0ff86ed4344d
sohaila <sohaila.namdari@gmail.com>
2021-08-02 10:13:29 +0430

    for pull

--- src/directives/can.js
+++ src/directives/can.js
@@ -12,14 +12,18 @@ const can = {
     if (vnode.context.$store.state.permission.isSet){
       let itemsProcessed = 0;
       binding.value.forEach(function (item) {
-        show = show || vnode.context.$store.state.permission.permissions.includes(item);
-        itemsProcessed++;
-        if (itemsProcessed === binding.value.length && !show ) {
+        if (item){
+          show = show || vnode.context.$store.state.permission.permissions.includes(item);
+          itemsProcessed++;
+          if (itemsProcessed === binding.value.length && !show ) {
 
-          vnode.elm.parentElement.removeChild(vnode.elm);
+            if(vnode.elm.parentElement) {
+              vnode.elm.parentElement.removeChild(vnode.elm);
+            }
 
-        } else {
-          elm.classList.remove('hidden');
+          } else {
+            elm.classList.remove('hidden');
+          }
         }
       })
     } else {
@@ -27,14 +31,16 @@ const can = {
 
         let itemsProcessed = 0;
         binding.value.forEach(function (item, index) {
-          show = show || permission.includes(item);
-          itemsProcessed++;
-          if (itemsProcessed === binding.value.length && !show ) {
-
-            vnode.elm.parentElement.removeChild(vnode.elm);
-
-          } else {
-            elm.classList.remove('hidden');
+          if (item) {
+            show = show || permission.includes(item);
+            itemsProcessed++;
+            if (itemsProcessed === binding.value.length && !show ) {
+              if(vnode.elm.parentElement) {
+                vnode.elm.parentElement.removeChild(vnode.elm);
+              }
+            } else {
+              elm.classList.remove('hidden');
+            }
           }
         })
 
